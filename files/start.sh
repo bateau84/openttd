21263dea738661882a8fbbfb99de98ae91b9d4e2
Mats Bøe Bergmann <Mats.Boe.Bergmann@nrk.no>
2015-03-05 14:46:07 +0100

    added conditions to loadgame and also savename

--- files/start.sh
+++ files/start.sh
@@ -5,9 +5,8 @@ savename=${savename="save/autosave/exit.sav"}
 if [[ $loadgame = true ]]; then
 	echo "We are loading a save game!"
 		echo "Lets load $savename"
-		#/usr/games/openttd -D -g "$savename" -x
-		#/usr/games/openttd -D -g "save/autosave/exit.sav" -x
+		/usr/games/openttd -D -g "save/autosave/exit.sav" -x
 else
 	echo "Dont load game"
-	#/usr/games/openttd -D -x
+	/usr/games/openttd -D -x
 fi
