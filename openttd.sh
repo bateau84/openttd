ada626a2442daf455440bc9c924893c1d5a361ec
Bateau <bateau@sea-shell.no>
2019-11-08 20:20:30 +0100

    changeuser

--- openttd.sh
+++ openttd.sh
@@ -7,6 +7,31 @@ savegame="${savepath}/${savename}"
 LOADGAME_CHECK="${loadgame}x"
 loadgame=${loadgame:-'false'}
 
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+PHOME=${PHOME:-"/home/openttd"}
+USER=${USER:-"openttd"}
+
+if [ ! "$(id -u ${USER})" -eq "$PUID" ]; then usermod -o -u "$PUID" ${USER} ; fi
+if [ ! "$(id -g ${USER})" -eq "$PGID" ]; then groupmod -o -g "$PGID" ${USER} ; fi
+if [ ! "$(grep ${USER} /etc/passwd | cut -d':' -f6)" == ${PHOME} ]; then
+        if [ ! -d ${PHOME} ]; then
+                mkdir -p ${PHOME}
+                chown ${USER}:${USER} ${PHOME}
+        fi
+        usermod -m -d ${PHOME} ${USER}
+fi
+
+echo "
+-----------------------------------
+GID/UID
+-----------------------------------
+User uid:    $(id -u ${USER})
+User gid:    $(id -g ${USER})
+User Home:   $(grep ${USER} /etc/passwd | cut -d':' -f6)
+-----------------------------------
+"
+
 # Loads the desired game, or prepare to load it next time server starts up!
 if [ ${LOADGAME_CHECK} != "x" ]; then
 
