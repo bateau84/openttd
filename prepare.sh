850cfa299562720353bfcf1dfb9db06858db1186
Mats Bergmann <bateau@sea-shell.org>
2022-04-27 17:05:25 +0200

    prepare for Ubuntu 22.04 jammy

--- prepare.sh
+++ prepare.sh
@@ -15,12 +15,14 @@ echo "deb http://security.ubuntu.com/ubuntu jammy-security main" >> /etc/apt/sou
 apt update -qq
 
 ## Install things we need
-$minimal_apt_get_install dumb-init wget unzip ca-certificates libfontconfig1 libfreetype6 libfluidsynth3 libicu-dev libpng16-16 liblzma-dev liblzo2-2 libsdl1.2debian libsdl2-2.0-0 > /dev/null 2>&1
+$minimal_apt_get_install dumb-init wget xz-utils unzip ca-certificates libfontconfig1 libfreetype6 libfluidsynth3 libicu-dev libpng16-16 liblzma-dev liblzo2-2 libsdl1.2debian libsdl2-2.0-0 > /dev/null 2>&1
 
 ## Download and install openttd
-wget -q https://cdn.openttd.org/openttd-releases/${OPENTTD_VERSION}/openttd-${OPENTTD_VERSION}-linux-${ID}-${UBUNTU_CODENAME}-amd64.deb
-dpkg -i openttd-${OPENTTD_VERSION}-linux-${ID}-${UBUNTU_CODENAME}-amd64.deb
-
+wget -q https://cdn.openttd.org/openttd-releases/${OPENTTD_VERSION}/openttd-${OPENTTD_VERSION}-linux-generic-amd64.tar.xz
+tar -xf openttd-${OPENTTD_VERSION}-linux-generic-amd64.tar.xz
+mkdir -p /usr/share/games/
+mv openttd-${OPENTTD_VERSION}-linux-generic-amd64 /usr/share/games/openttd
+rm openttd-${OPENTTD_VERSION}-linux-generic-amd64.tar.xz
 
 ## Download GFX and install
 mkdir -p /usr/share/games/openttd/baseset/
