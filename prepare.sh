7753890a0630c6f7164d4e18f7301735eb2e3854
Rus <russellwilson01@hotmail.com>
2023-02-09 17:51:55 +0000

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -26,6 +26,10 @@ mkdir /home/openttd/gamefiles
 chown openttd /home/openttd/gamefiles
 wget "https://cdn.openttd.org/openttd-releases/13.0/openttd-13.0-linux-generic-amd64.tar.xz" -P /home/openttd/
 tar -xvf /home/openttd/openttd-13.0-linux-generic-amd64.tar.xz --directory /home/openttd/gamefiles/
+
+wget -q -O opengfx-${OPENGFX_VERSION}.zip https://cdn.openttd.org/opengfx-releases/${OPENGFX_VERSION}/opengfx-${OPENGFX_VERSION}-all.zip
+unzip opengfx-${OPENGFX_VERSION}.zip -d /home/openttd/gamefiles/baseset/
+
 chown openttd /home/openttd/gamefiles
 
 ## Set entrypoint script to right user
